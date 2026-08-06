e7fbd572e151cd97cf6cbf4f40ffb765730e4f14
Ishan <ishannbhusal3@gmail.com>
2026-08-06 18:13:45 +0545

    Add chatbot interface to home page and update URL routing for chatbot functionality

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from advisory import views
@@ -14,6 +14,7 @@ urlpatterns = [
     path("weather-data/", views.weather_data_view, name="weather_data"),
     path("about/", views.about_view, name="about"),
     path("logout/", views.logout_view, name="logout"),
+    path("bert_bot/", include("bert_bot.urls")),
     # path("chatbot/", views.chatbot_view, name="chatbot"),
     # path("chatbot/message/", views.chatbot_message, name="chatbot_message"),
 ]
